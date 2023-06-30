a0d6efe3fd2680c8341b3a475bfec5e57f3e801d
DenisNgaitia <dennisngatiantlax@gmail.com>
2023-06-30 14:38:30 -0700

    fixed files for betty style

--- get_print_func.c
+++ get_print_func.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_print_func - fuction that selects correct function to perform the operation.
+ * get_print_func - fuction selects correct function to perform the operation.
  * @s: this is the argument identifier
  * @index: this is the index for argument identifier
  * Return: this is the pointer to a function.
@@ -10,26 +10,16 @@
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 {
 	print_t pr[] = {
-		{"c", print_chr},
-		{"s", print_str},
-		{"i", print_int},
-		{"d", print_int},
-		{"b", print_bnr},
-		{"u", print_unt},
-		{"o", print_oct},
-		{"x", print_hex},
-		{"X", print_upx},
-		{"S", print_usr},
-		{"p", print_add},
-		{"li", prinlint},
-		{"ld", prinlint},
-		{"lu", prinlunt},
-		{"lo", prinloct},
-		{"lx", prinlhex},
-		{"lX", prinlupx},
-		{"hi", prinhint},
-		{"hd", prinhint},
-		{"hu", prinhunt},
+		{"c", print_chr}, {"x", print_hex},
+		{"X", print_upx}, {"S", print_usr},
+		{"p", print_add}, {"li", prinlint},
+		{"ld", prinlint}, {"lu", prinlunt},
+		{"lo", prinloct}, {"lx", prinlhex},
+		{"lX", prinlupx}, {"hi", prinhint},
+		{"hd", prinhint}, {"s", print_str},
+		{"i", print_int}, {"d", print_int},
+		{"b", print_bnr}, {"u", print_unt},
+		{"o", print_oct}, {"hu", prinhunt},
 		{"ho", prinhoct},
 		{"hx", prinhhex},
 		{"hX", prinhupx},
@@ -63,7 +53,7 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 		{" %", print_prg},
 		{NULL, NULL},
 	};
-	
+
 	int i = 0, j = 0, first_index = index;
 
 	while (pr[i].type_arg)
