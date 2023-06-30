a6e8b4485705d1b7722cec87eaf2e3a00f8decb7
OBN1 <nyangaresibrian@gmail.com>
2023-06-30 16:20:57 -0700

    Amended the ev_print_func.c

---
+++ ev_print_func.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * ev_print_func - Returns the amount fo identifies
+ * @s: argument identifier
+ * @index: index of argument identifier
+ * Return: amount of indentifiers
+ */
+int ev_print_func(const char *s, int index)
+{
+	print_t pr[] = {
+		{"c", print_chr},
+		{"s", print_str},
+		{"%", print_prg}
+	};
+	int i = 0, j = 0, first_ndex;
+
+	first_index = index;
+	while (pr[i].type_arg)
+	{
+		if (s[index] == pr[i].type_arg[j])
+		{
+			if (pr[i].type_arg[j + 1] != '\0')
+				index++, j++;
+			else
+				break;
+		}
+		else
+		{
+			j = 0;
+			i++;
+			index = first_index;
+		}
+	}
+	return (j);
+}
