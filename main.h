3b7fe62ca2bd8cf92a67efb08619b63ada51014e
OBN1 <nyangaresibrian@gmail.com>
2023-06-30 15:34:50 -0700

    Added the hex function declared in the get func functions

--- main.h
+++ main.h
@@ -24,4 +24,5 @@ int print_str(va_list arguments, char *buf, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int print_hex(va_list arguments, char *buf, unsigned int ibuf);
 #endif
